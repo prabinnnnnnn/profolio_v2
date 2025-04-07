7a983c66b86b9efe325fa80373103046a59adf9c
prabinnnnnn <mprabintamang@gmail.com>
2025-04-07 16:15:37 +0545

    drop: remove the footer

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/providers/theme-context";
 import ReactLenis from "lenis/react";
 import type { Metadata } from "next";
 import { ScrollToHash } from "@/components/scroll-to-hash";
+import { Inter } from "next/font/google";
 
 import "@/styles/globals.css";
 import Navbar from "@/components/navbar/navbar";
@@ -11,44 +12,52 @@ import Footer from "@/components/footer";
 
 import { config } from "@/config";
 
+const inter = Inter({
+    subsets: ["latin"],
+});
+
 export const metadata: Metadata = {
-  title: `${config.name} | Mern Stack Developer`,
-  description:
-    "I'm a mern stack developer with a passion for building web applications.",
+    title: `${config.name} | Mern Stack Developer`,
+    description:
+        "I'm a mern stack developer with a passion for building web applications.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  const lenisOptions = {
-    lerp: 0.1,
-    duration: 1.5,
-    smoothTouch: false,
-    smooth: true,
-  };
+    const lenisOptions = {
+        lerp: 0.1,
+        duration: 1.5,
+        smoothTouch: false,
+        smooth: true,
+    };
 
-  return (
-    <html lang="en" className="w-screen  dark" suppressHydrationWarning={true}>
-      <head>
-        <link rel="icon" href="/logo/circle.svg" sizes="any" />
-      </head>
-      <ReactLenis root options={lenisOptions}>
-        <ThemeProvider>
-          <body
-            className={`antialiased relative w-full h-fit`}
-            cz-shortcut-listen="true"
-          >
-            <Navbar />
-            <MobileNavbar />
-            <ScrollToHash />
-            {children}
-            <GoBackUp />
-            <Footer />
-          </body>
-        </ThemeProvider>
-      </ReactLenis>
-    </html>
-  );
+    return (
+        <html
+            lang="en"
+            className="w-screen  dark"
+            suppressHydrationWarning={true}
+        >
+            <head>
+                <link rel="icon" href="/logo/circle.svg" sizes="any" />
+            </head>
+            <ReactLenis root options={lenisOptions}>
+                <ThemeProvider>
+                    <body
+                        className={`${inter.className} antialiased relative w-full h-fit`}
+                        cz-shortcut-listen="true"
+                    >
+                        <Navbar />
+                        <MobileNavbar />
+                        <ScrollToHash />
+                        {children}
+                        <GoBackUp />
+                        {/* <Footer /> */}
+                    </body>
+                </ThemeProvider>
+            </ReactLenis>
+        </html>
+    );
 }
