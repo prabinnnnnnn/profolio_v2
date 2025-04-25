7a2af872e1fd7fb3b476c60df8b9bc1b16472311
prabinnnnnn <mprabintamang@gmail.com>
2025-04-25 11:28:24 +0545

    Feat: add the certification of docker  & remove unnessery logo

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["github.com", "res.cloudinary.com", "images.unsplash.com"],
+    domains: ["github.com", "res.cloudinary.com", "images.unsplash.com", "prabinmoktan.com"],
   },
 };
 
